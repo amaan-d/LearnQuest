2f117d574bb927a36767c3c74ea04fbd4549e178
amaan-d <dhanani.amaan0@gmail.com>
2025-02-23 16:19:23 +0000

    removed accidential changes by making new repo

---
+++ LearnQuest/src/routes/signup/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+  import Signup from "$components/Signup.svelte";
+  import { Card } from "@sveltestrap/sveltestrap";
+</script>
+
+<style>
+  .center-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh; /* Full viewport height */
+    padding: 15px;
+    background-color: black;
+  }
+
+
+</style>
+
+<div class="center-container">
+    <Signup/>
+</div>
